68cb6df4eb2abf25f74cdcf8adf1c24ce6392a44
subhamanna-98 <subhamanna559@gmail.com>
2025-10-31 19:16:51 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,83 @@
+const closeBtn = document.querySelector(".close-btn");
+const navbarCollesped = document.querySelector(".navbar-collesped");
+const body = document.querySelector(".body");
+const overLay = document.querySelector(".over-lay");
+let close = true;
+
+closeBtn.addEventListener("click", () =>{
+    if(close==true){
+        close = false;
+        navbarCollesped.style.right = "0%";
+        closeBtn.classList.add("open");
+        body.style.overflow = "hiden";
+        overLay.classList.add("active");
+    }else{
+        close = true;
+        navbarCollesped.style.right = "-100%";
+        closeBtn.classList.remove("open");
+        body.style.overflow = "visible";
+        overLay.classList.remove("active");
+    }
+});
+
+ var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+       spaceBetween: 30,
+      loop: true,
+      // navigation: {
+      //   nextEl: ".swiper-button-next",
+      //   prevEl: ".swiper-button-prev",
+      // },
+      pagination: {
+        el: ".swiper-pagination",
+         clickable: true,
+      },
+      mousewheel: true,
+      keyboard: true,
+      breakpoints: {
+        1700:{
+             slidesPerView: 3,
+        },
+    1470: {
+      slidesPerView: 2,
+    },
+    992: {
+      slidesPerView: 1,
+    },
+},
+    });
+
+
+    // counter-sec
+    const counters = document.querySelectorAll(".counter");
+
+  const runCounter = (counter) => {
+    counter.innerText = "0";
+    const target = +counter.getAttribute("data-target");
+    const speed = 500; // lower = faster
+
+    const update = () => {
+      const value = +counter.innerText;
+      const increment = Math.ceil(target / speed);
+      if (value < target) {
+        counter.innerText = value + increment;
+        setTimeout(update, 30);
+      } else {
+        counter.innerText = target;
+      }
+    };
+
+    update();
+  };
+
+  // Counter starts only when visible on screen
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        runCounter(entry.target);
+        observer.unobserve(entry.target);
+      }
+    });
+  }, { threshold: 0.5 });
+
+  counters.forEach(counter => observer.observe(counter));
